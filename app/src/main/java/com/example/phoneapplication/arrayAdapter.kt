8d594b6cec1b602a237ef1a7a5e304012c3b5504
annmcclure <amcclu13@vols.utk.edu>
2023-04-08 23:42:45 -0400

    swap activity where you will add the user and matching components as well

---
+++ app/src/main/java/com/example/phoneapplication/arrayAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.phoneapplication
+
+class arrayAdapter {
+}
---
+++ app/src/main/java/com/example/phoneapplication/swap_activity.kt
@@ -0,0 +1,191 @@
+package com.example.phoneapplication
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import com.lorentzos.flingswipe.SwipeFlingAdapterView.onFlingListener
+import com.example.phoneapplication.arrayAdapter;
+import com.example.phoneapplication.cards;
+
+class swap_activity : AppCompatActivity(){
+    private val cards_data: Array<cards>
+    private var arrayAdapter: com.example.phoneapplication.arrayAdapter? = null
+    private val i = 0
+    private lateinit var auth: FirebaseAuth
+    private var currentUId: String? = null
+    //private var usersDb: DatabaseReference? = null
+
+    var listView: ListView? = null
+    var rowItems: MutableList<cards>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swap)
+        //usersDb = FirebaseDatabase.getInstance().getReference().child("Users")
+        //mAuth = FirebaseAuth.getInstance()
+        //currentUId = mAuth!!.currentUser!!.uid
+        //checkUserSex()
+        rowItems = ArrayList<cards>()
+        arrayAdapter = arrayAdapter(this, R.layout.item, rowItems)
+        val flingContainer = findViewById<View>(R.id.frame) as SwipeFlingAdapterView
+        flingContainer.adapter = arrayAdapter
+        flingContainer.setFlingListener(object : onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                Log.d("LIST", "removed object!")
+                rowItems!!.removeAt(0)
+                arrayAdapter.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(dataObject: Any) {
+                val obj: cards = dataObject as cards
+                val userId: String? = obj.getUserId()
+                //usersDb.child(userId).child("connections").child("nope").child(currentUId)
+                //    .setValue(true)
+                Toast.makeText(this@swap_activity, "Left", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(dataObject: Any) {
+                val obj: cards = dataObject as cards
+                val userId: String? = obj.getUserId()
+                //usersDb.child(userId).child("connections").child("yeps").child(currentUId)
+                //    .setValue(true)
+                //isConnectionMatch(userId)
+                Toast.makeText(this@swap_activity, "Right", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {}
+            override fun onScroll(scrollProgressPercent: Float) {}
+        });
+
+        // additional from the video that we havent gotten to yet (matches, users profile, etc)
+        /*
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainActivity.this, "Item Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void isConnectionMatch(String userId) {
+        DatabaseReference currentUserConnectionsDb = usersDb.child(currentUId).child("connections").child("yeps").child(userId);
+        currentUserConnectionsDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    Toast.makeText(MainActivity.this, "new Connection", Toast.LENGTH_LONG).show();
+
+                    String key = FirebaseDatabase.getInstance().getReference().child("Chat").push().getKey();
+
+                    usersDb.child(dataSnapshot.getKey()).child("connections").child("matches").child(currentUId).child("ChatId").setValue(key);
+                    usersDb.child(currentUId).child("connections").child("matches").child(dataSnapshot.getKey()).child("ChatId").setValue(key);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private String userSex;
+    private String oppositeUserSex;
+    public void checkUserSex(){
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userDb = usersDb.child(user.getUid());
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    if (dataSnapshot.child("sex").getValue() != null){
+                        userSex = dataSnapshot.child("sex").getValue().toString();
+                        switch (userSex){
+                            case "Male":
+                                oppositeUserSex = "Female";
+                                break;
+                            case "Female":
+                                oppositeUserSex = "Male";
+                                break;
+                        }
+                        getOppositeSexUsers();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void getOppositeSexUsers(){
+        usersDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.child("sex").getValue() != null) {
+                    if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("nope").hasChild(currentUId) && !dataSnapshot.child("connections").child("yeps").hasChild(currentUId) && dataSnapshot.child("sex").getValue().toString().equals(oppositeUserSex)) {
+                        String profileImageUrl = "default";
+                        if (!dataSnapshot.child("profileImageUrl").getValue().equals("default")) {
+                            profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
+                        }
+                        cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString(), profileImageUrl);
+                        rowItems.add(item);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+
+    public void logoutUser(View view) {
+        mAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, ChooseLoginRegistrationActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+    }
+
+    public void goToSettings(View view) {
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(intent);
+        return;
+    }
+
+    public void goToMatches(View view) {
+        Intent intent = new Intent(MainActivity.this, MatchesActivity.class);
+        startActivity(intent);
+        return;
+    }
+        */
+
+    }
+}
