6d2d9e0435a1020f1c318aa5d51c409b79c7773e
annmcclure <amcclu13@vols.utk.edu>
2023-04-08 23:43:46 -0400

    cards info

--- app/src/main/java/com/example/phoneapplication/cards.kt
+++ app/src/main/java/com/example/phoneapplication/cards.kt
@@ -1,4 +1,38 @@
 package com.example.phoneapplication
 
-class cards {
+import androidx.appcompat.app.AppCompatActivity
+
+class cards : AppCompatActivity() {
+    private var userId: String? = null
+    private var name: String? = null
+    private var profileImageUrl: String? = null
+    fun cards(userId: String?, name: String?, profileImageUrl: String?) {
+        this.userId = userId
+        this.name = name
+        this.profileImageUrl = profileImageUrl
+    }
+
+    fun getUserId(): String? {
+        return userId
+    }
+
+    fun setUserID(userID: String?) {
+        userId = userId
+    }
+
+    fun getName(): String? {
+        return name
+    }
+
+    fun setName(name: String?) {
+        this.name = name
+    }
+
+    fun getProfileImageUrl(): String? {
+        return profileImageUrl
+    }
+
+    fun setProfileImageUrl(profileImageUrl: String?) {
+        this.profileImageUrl = profileImageUrl
+    }
 }
