6ac509c5b1d0af7658ce3b62567154256362e561
Pooja Masani <poojamasani9@gmail.com>
2023-04-14 21:57:31 -0400

    Added age,gender,genderpref

--- app/src/main/java/com/example/phoneapplication/MainActivity.kt
+++ app/src/main/java/com/example/phoneapplication/MainActivity.kt
@@ -1,26 +1,44 @@
 package com.example.phoneapplication
 
-import android.content.Intent
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
+
+data class User(
+    val userId: String = "",
+    val name: String = "",
+    val email: String = "",
+    val sex: String = "",
+    val age: Int = 0,
+    val gender: String = "",
+    val genderPref: String = ""
+) {
+    // add other methods as needed
+}
 
 class MainActivity : AppCompatActivity() {
-  //  private lateinit var button: Button
-  companion object {
-      private const val RC_SIGN_IN = 9001
-  }
+    //  private lateinit var button: Button
+    val database = Firebase.database.reference
+    val usersRef = database.child("users")
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,12 +65,13 @@ class MainActivity : AppCompatActivity() {
         launcher.launch(signInIntent)
     }
 
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            handleResults(task)
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
         }
-    }
 
     private fun handleResults(task: Task<GoogleSignInAccount>) {
         if (task.isSuccessful) {
@@ -70,15 +89,54 @@ class MainActivity : AppCompatActivity() {
         val credential = GoogleAuthProvider.getCredential(account.idToken, null)
         auth.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                val intent = Intent(this, main_menu::class.java)
-                intent.putExtra("email", account.email)
-                intent.putExtra("name", account.displayName)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                val userId = auth.currentUser?.uid ?: ""
+                val name = account.displayName ?: ""
+                val email = account.email ?: ""
+                val gender = "" // replace this with the user's selected gender
+                val age = 0 // replace this with the user's selected age
+
+                // Add the user to the Firebase Realtime Database
+                val user = User(
+                    userId = userId,
+                    name = name,
+                    email = email,
+                    sex = "",
+                    age = 0,
+                    gender = gender,
+                    genderPref = ""
+                )
+
+                usersRef.child(userId).setValue(user).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "User added to database", Toast.LENGTH_SHORT).show()
+                        setupUserListener()
+                    } else {
+                        Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
     }
+
+    private fun setupUserListener() {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            val userRef = usersRef.child(userId)
+            userRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.getValue(User::class.java)
+                    if (user != null) {
+                        // Do something with the user data
+                    }
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error
+                }
+            })
+        }
+    }
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -90,10 +148,9 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, main_menu::class.java)
         startActivity(intent)
     }
-
 }
-    //}
-    //connects to next page
+//
+//   // connects to next page
 //    private fun setButton() {
 //        button = findViewById<Button>(R.id.button)
 //        button.setOnClickListener {
--- app/src/main/java/com/example/phoneapplication/MainActivity2.kt
+++ app/src/main/java/com/example/phoneapplication/MainActivity2.kt
@@ -1,33 +1,61 @@
 package com.example.phoneapplication
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.TextView
-import com.google.firebase.auth.FirebaseAuth
 import android.content.Intent
-import android.widget.Button
-import com.example.phoneapplication.MainActivity2
-import com.example.phoneapplication.R
-
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    @SuppressLint("MissingInflatedId")
+    private lateinit var database: DatabaseReference
+    private lateinit var userId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+        userId = auth.currentUser!!.uid
 
         val email = intent.getStringExtra("email")
         val displayName = intent.getStringExtra("name")
 
-        findViewById<TextView>(R.id.textView2).text = email + "\n" + displayName
+       // findViewById<TextView>(R.id.textView2).text = "$email\n$displayName"
+
+        findViewById<Button>(R.id.gSignOutBtn).setOnClickListener{
+            val age = findViewById<EditText>(R.id.ageEditText).text.toString().toIntOrNull()
+            val sex = findViewById<Spinner>(R.id.genderSpinner).selectedItem.toString()
+            val genderPref = findViewById<Spinner>(R.id.preferredGenderSpinner).selectedItem.toString()
+
+            if (age == null) {
+                Toast.makeText(this, "Please enter a valid age", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val userUpdates = hashMapOf<String, Any>(
+                "age" to age,
+                "sex" to sex,
+                "genderPref" to genderPref
+            )
+
+            database.child("users").child(userId).updateChildren(userUpdates).addOnSuccessListener {
+                Toast.makeText(this, "User data updated", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+            }.addOnFailureListener {
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         findViewById<Button>(R.id.gSignOutBtn).setOnClickListener{
             auth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
