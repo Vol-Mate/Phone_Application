983220d63cd4f9eaedbbb93ff23aa95c263dc07b
annmcclure <amcclu13@vols.utk.edu>
2023-04-22 14:14:05 -0400

    still working

--- app/src/main/java/com/example/phoneapplication/swap_activity.kt
+++ app/src/main/java/com/example/phoneapplication/swap_activity.kt
@@ -6,162 +6,144 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.annotation.Keep
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.auth.User
 import com.lorentzos.flingswipe.SwipeFlingAdapterView
 import com.lorentzos.flingswipe.SwipeFlingAdapterView.onFlingListener
-import com.example.phoneapplication.arrayAdapter;
-import com.example.phoneapplication.cards;
+import com.example.phoneapplication.arrayAdapter
+import com.example.phoneapplication.cards
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+@Keep
+class User3(
+    val userId: String = "",
+    val name: String = "",
+    val email: String = "",
+    val age: Int = 0,
+    val gender: String = "",
+    val genderPref: String = ""
+) {
+    // add other methods as needed
+    constructor() : this("", "", "", 0,"", "")
+}
 
 open class swap_activity : AppCompatActivity(){
-    lateinit var cards_data: Array<cards>
-    var arrayAdapter: arrayAdapter? = null
-    val i = 0
-    lateinit var auth: FirebaseAuth
-    var currentUId: String? = null
-    var usersDb: DatabaseReference? = null
-
-    var listView: ListView? = null
-    var rowItems: MutableList<cards>? = null
+    val database = Firebase.database("https://phone-application-14522.firebaseio.com/")
+    val usersRef = database.getReference("users")
+    private lateinit var cards: Array<cards>
+    private lateinit var arrayAdapter: arrayAdapter
+    private var i: Int = 0
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var listView: ListView
+    private lateinit var rowItems: MutableList<cards>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_swap)
-        usersDb = FirebaseDatabase.getInstance().reference.child("users")
-        auth = FirebaseAuth.getInstance()
-        currentUId = auth.currentUser?.uid
-        checkUserSex()
-        rowItems = ArrayList<cards>()
-        arrayAdapter = arrayAdapter(this, R.layout.item, rowItems as ArrayList<cards>)
-        val flingContainer = findViewById<View>(R.id.frame) as SwipeFlingAdapterView
-        flingContainer.adapter = arrayAdapter
-        flingContainer.setFlingListener(object : onFlingListener {
-            override fun removeFirstObjectInAdapter() {
-                Log.d("LIST", "removed object!")
-                (rowItems as ArrayList<cards>).removeAt(0)
-                arrayAdapter?.notifyDataSetChanged()
-            }
 
-            fun DatabaseReference.childSafe(child: String): DatabaseReference? {
-                return this.child(child)
-            }
+        mAuth = FirebaseAuth.getInstance()
+        val userId = mAuth.currentUser?.uid
 
-            override fun onLeftCardExit(dataObject: Any) {
-                val obj: cards = dataObject as cards
-                val userId: String? = obj.userId
-                userId?.let {
-                    usersDb?.child(it)?.child("connections")?.child("nope")
-                        ?.child(currentUId!!)
-                        ?.setValue(true)
-                }
-                Toast.makeText(this@swap_activity, "Left", Toast.LENGTH_SHORT).show()
-            }
+        if (userId != null) {
+            val userRef = usersRef.child(userId)
+            //checkUserSex()
 
-            override fun onRightCardExit(dataObject: Any) {
-                val obj: cards = dataObject as cards
-                val userId: String? = obj.getUserId()
-                userId?.let {
-                    usersDb?.child(it)?.child("connections")?.child("yeps")
-                        ?.child(currentUId!!)
-                        ?.setValue(true)
-                }
-                //isConnectionMatch(userId.toString())
-                Toast.makeText(this@swap_activity, "Right", Toast.LENGTH_SHORT).show()
-            }
+            rowItems = ArrayList()
 
-            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {}
-            override fun onScroll(scrollProgressPercent: Float) {}
-        });
-    }
+            arrayAdapter = arrayAdapter(this, R.layout.item, rowItems)
 
-        private var userSex: String? = null
-        private var oppositeUserSex: String? = null
+            val flingContainer = findViewById<View>(R.id.frame) as SwipeFlingAdapterView
 
-        private fun checkUserSex() {
-            val user = FirebaseAuth.getInstance().currentUser
-            val userDb = usersDb?.child(user?.uid ?: "")
-            userDb?.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        if (dataSnapshot.child("sex").getValue() != null) {
-                            userSex = dataSnapshot.child("sex").getValue().toString()
-                            when (userSex) {
-                                "Male" -> oppositeUserSex = "Female"
-                                "Female" -> oppositeUserSex = "Male"
-                            }
-                            getOppositeSexUsers()
-                        }
-                    }
+            flingContainer.adapter = arrayAdapter
+            flingContainer.setFlingListener(object : onFlingListener {
+                override fun removeFirstObjectInAdapter() {
+                    Log.d("LIST", "removed object!")
+                    rowItems.removeAt(0)
+                    arrayAdapter.notifyDataSetChanged()
                 }
 
-                override fun onCancelled(databaseError: DatabaseError) {
-
-                }
-            })
-        }
-
-        private fun getOppositeSexUsers() {
-            usersDb?.addChildEventListener(object : ChildEventListener {
-                override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
-                    if (dataSnapshot.child("sex").getValue() != null) {
-                        if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("nope").hasChild(currentUId!!) && !dataSnapshot.child("connections").child("yeps").hasChild(
-                                currentUId!!
-                            ) && dataSnapshot.child("sex").getValue().toString() == oppositeUserSex) {
-                            var answer = "default"
-                            if (!dataSnapshot.child("answer").getValue()?.equals("default")!!) {
-                                answer = dataSnapshot.child("answer").getValue().toString()
-                            }
-                            val item = dataSnapshot.key?.let { cards(it, dataSnapshot.child("name").getValue().toString(), answer) }
-                            item?.let { (rowItems as ArrayList<cards>).add(it) }
-                            arrayAdapter?.notifyDataSetChanged()
-                        }
-                    }
+                override fun onLeftCardExit(dataObject: Any) {
+                    val obj = dataObject as cards
+                    val user = obj.getUserId()
+                    userRef.child(userId).child("connections").child("nope").child(user)
+                        .setValue(true)
+                    Toast.makeText(this@swap_activity, "Left", Toast.LENGTH_SHORT).show()
                 }
 
-                override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
-
+                override fun onRightCardExit(dataObject: Any) {
+                    val obj = dataObject as cards
+                    val user = obj.getUserId()
+                    userRef.child(userId).child("connections").child("yeps").child(user)
+                        .setValue(true)
+                    //isConnectionMatch(userId)
+                    Toast.makeText(this@swap_activity, "Right", Toast.LENGTH_SHORT).show()
                 }
 
-                override fun onChildRemoved(dataSnapshot: DataSnapshot) {
-
-                }
-
-                override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {
-
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-
-                }
+                override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {}
+                override fun onScroll(scrollProgressPercent: Float) {}
             })
 
-            /*private fun isConnectionMatch(userId: String) {
-            val currentUserConnectionsDb = usersDb?.child(currentUId!!)?.child("connections")?.child("yeps")?.child(userId)
-            currentUserConnectionsDb?.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        Toast.makeText(this@swap_activity, "new Connection", Toast.LENGTH_LONG).show()
-
-                        val key = FirebaseDatabase.getInstance().reference.child("Chat").push().key
-
-                        usersDb?.child(dataSnapshot.key!!)?.child("connections")?.child("matches")?.child(currentUId!!)?.child("ChatId")?.setValue(key)
-                        usersDb?.child(currentUId!!)?.child("connections")?.child("matches")?.child(dataSnapshot.key!!)?.child("ChatId")?.setValue(key)
-                    }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                }
-            })
-        }*/
+            // Optionally add an OnItemClickListener
+            flingContainer.setOnItemClickListener { _, _, _, _ ->
+                Toast.makeText(this@swap_activity, "Item Clicked", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+    private lateinit var userSex: String
+    private lateinit var oppositeUserSex: String
+
+    private fun checkUserSex() {
+        val user = FirebaseAuth.getInstance().currentUser
+        val userRef = usersRef.child(user?.uid ?: "")
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val user = dataSnapshot.getValue(User3::class.java)
+                    user?.let {
+                        userSex = user.gender
+                        oppositeUserSex = user.genderPref
+                        getOppositeSexUsers()
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun getOppositeSexUsers() {
+        mAuth = FirebaseAuth.getInstance()
+        val userId = mAuth.currentUser?.uid
+        usersRef.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+                val user = dataSnapshot.getValue(User3::class.java)
+                if (user != null && user.gender == oppositeUserSex && dataSnapshot.key != userId) {
+                    if (!dataSnapshot.child("connections").child("nope").hasChild(userId.toString())
+                        && !dataSnapshot.child("connections").child("yeps").hasChild(userId.toString())) {
+                        val item = cards(dataSnapshot.key ?: "", user.name, "default")
+                        rowItems.add(item)
+                        arrayAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {}
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {}
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+}
+
