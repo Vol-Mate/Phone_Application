79ce570429b62727f4583528e2a96eb567ff2c7e
Isha Bhandari <isha.bhandari101@gmail.com>
2023-05-10 22:23:43 -0400

    nayanas comments

--- app/src/main/java/com/example/phoneapplication/NotificationReceiver.kt
+++ app/src/main/java/com/example/phoneapplication/NotificationReceiver.kt
@@ -12,7 +12,8 @@ const val channelID = "channel1"
 const val titleExtra = "titleExtra"
 const val messageExtra = "messageExtra"
 
-
+// this part brings the application into action by either creating
+// a status bar notification or performing a task
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -20,39 +21,10 @@ class NotificationReceiver : BroadcastReceiver() {
             .setSmallIcon(R.drawable.notification_icon)
             .setContentTitle("Have a nice date!")
             .setContentText("Have a nice date!")
-             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
 
         with(NotificationManagerCompat.from(context)) {
             notify(1, builder.build())
         }
     }
 }
-
-//package code.with.cal.localnotificationstutorial
-//
-//import android.app.NotificationManager
-//import android.content.BroadcastReceiver
-//import android.content.Context
-//import android.content.Intent
-//import androidx.core.app.NotificationCompat
-//
-//const val notificationID = 1
-//const val channelID = "channel1"
-//const val titleExtra = "titleExtra"
-//const val messageExtra = "messageExtra"
-//
-//class Notification : BroadcastReceiver()
-//{
-//    override fun onReceive(context: Context, intent: Intent)
-//    {
-//        val notification = NotificationCompat.Builder(context, channelID)
-//            .setSmallIcon(R.drawable.ic_launcher_foreground)
-//            .setContentTitle(intent.getStringExtra(titleExtra))
-//            .setContentText(intent.getStringExtra(messageExtra))
-//            .build()
-//
-//        val  manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//        manager.notify(notificationID, notification)
-//    }
-//
-//}
